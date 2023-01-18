ff5114491d9b623056e75dfa412fc21a5a795ae8
talhayaseen57 <talhayaseen5725@gmail.com>
2023-01-18 19:29:25 +0100

    ternary operator

--- practice_C/boolean_datatypes.c
+++ practice_C/boolean_datatypes.c
@@ -26,6 +26,8 @@ int main() {
     printf("Enter a number: ");
     scanf("%lf", &number);
 
+    // using if else condictions
+    printf("Using if-else conditions:\n");
     if (number < 0) {
         printf("The given number is negative.\n");
     }
@@ -33,8 +35,16 @@ int main() {
         printf("The given number is 0.\n");
     }
     else {
-        printf("The given number is positive.\n");
+        printf("The given number is positive.\n\n");
     }
+
+    /*
+    Ternary operator:
+    test_condition ? (if true)statement1 : (if false)statement2;
+    */
+    printf("Using ternary operator:\n");
+    (number < 0) ? printf("The given number is negative.\n") : (number == 0) ? 
+    printf("The given number is 0.\n") : printf("The given number is positive.\n");
 
     return 0;
 }
